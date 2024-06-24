037846b56b4479a75b84e5fd763b915bc8da732c
Carolina Lobato <carolinabclobato@gmail.com>
2024-06-24 15:37:47 +0200

    updates

--- scripts/r/archive/03_cluster-analysis.R
+++
@@ -1,9 +0,0 @@
-# Silhoute plot ggplot 
-# https://rpkgs.datanovia.com/factoextra/reference/fviz_silhouette.html
-library(cluster)
-
-read.csv("outputs/umap/")
-embeds <- 
-
-s <- silhouette(labels, dist(embeds, method="euclidean"), full=TRUE)
-
